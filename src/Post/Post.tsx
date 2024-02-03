a0777a554f7ec47b4946841d158ac270731aeac8
Dmitry Pilavskiy <saw.coding@gmail.com>
2024-02-03 07:38:02 +0300

    commit - 13

---
+++ src/Post/Post.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div>
+      <h3>Post</h3>
+    </div>
+  );
+};
+
+export default Post;
--- src/Test/Test.tsx
+++ src/Test/Test.tsx
@@ -4,6 +4,10 @@ const Test = () => {
   return (
     <div>
       <h2>Test555</h2>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, animi, cum. Accusamus consequatur deserunt
+        explicabo fugit illum in ipsum itaque laudantium maiores mollitia necessitatibus nisi nulla, numquam recusandae
+        totam! Vero!
+      </h2>
     </div>
   );
 };
